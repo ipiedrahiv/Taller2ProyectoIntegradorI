e9a488aa3193a76435c34d39d6a4e5cc3eabf75e
Isabel Piedrahita <ipiedrahiv@eafit.edu.co>
2020-05-19 16:32:46 -0500

    Taller 4

--- measure/views.py
+++ measure/views.py
@@ -6,8 +6,7 @@ from django.shortcuts import get_object_or_404, render
 import requests
 
 def measure(request):
-     # Verifica si hay un parámetro value en la petición POST
-    if request.method == "POST":  # when user sends registration info:
+    if request.method == "POST":
         args = {
             'type' : 'mayo12',
             'codigo' : request.POST['codigo'],
@@ -18,15 +17,10 @@ def measure(request):
             'area' : request.POST['area'],
             'terreno' : request.POST['terreno'],
         }
-        # Crea el json para realizar la petición POST al Web Service
         print(args)
         response = requests.post('http://127.0.0.1:8000/measure/', args)
-        # Convierte la respuesta en JSON
         measure_json = response.json()
 
-    # Realiza una petición GET al Web Services
     response = requests.get('http://127.0.0.1:8000/measure/')
-    # Convierte la respuesta en JSON
     measures = response.json()
-    # Rederiza la respuesta en el template measure
     return render(request, "measure/measure.html", {'measures': measures})
